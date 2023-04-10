00abb745aca51e1bdc7d63f22d50246a88713e70
symonogwe <symonogwe@gmail.com>
2023-04-10 09:27:54 +0300

    Edit playROund function added conditions for results of playing a single round.

--- project.js
+++ project.js
@@ -6,9 +6,22 @@ function getComputerChoice() {
     return choice;
 }
 
-function playGame(playerSelection, computerSelection) {
-    if (playerSelection === "rock" && computerSelection === "paper")
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection === "rock") && (computerSelection === "Paper"))
       return "You Loose! Paper beats Rock";   
+    if ((playerSelection === "paper") && (computerSelection === "Rock"))
+      return "You Win! Paper beats Rock";
+    if ((playerSelection === "rock") && (computerSelection === "Scissors"))
+      return "You Win! rock beats Scissors";
+    if ((playerSelection === "scissors") && (computerSelection === "Rock"))
+      return "You Loose! Rock beats scissors";
+    if ((playerSelection === "scissors") && (computerSelection === "Paper"))
+      return "You Win! Scissors beats paper";
+    if ((playerSelection === "paper") && (computerSelection === "Scissors"))
+      return "You Loose! Scissors beats paper";
+    
 }
 
-console.log(getComputerChoice());
+
+
+
