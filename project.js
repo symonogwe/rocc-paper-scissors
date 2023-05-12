b6807ab6d97158bb53a424666699ff4fc79bed59
symonogwe <symonogwe@gmail.com>
2023-05-12 16:38:03 +0300

    Add score to player and computer

--- project.js
+++ project.js
@@ -1,11 +1,12 @@
 
-
+// computer choice
 function getComputerChoice() {
   const compChoice = ["Rock", "Paper", "Scissors"];
   let choice = compChoice[Math.floor(Math.random() * compChoice.length)];
   return choice;
 }
 
+//result for a single round played
 function playRound(playerSelection, computerSelection) {
   if ((playerSelection === "rock") && (computerSelection === "Paper"))
     return "You Loose! Paper beats Rock";   
@@ -23,39 +24,37 @@ function playRound(playerSelection, computerSelection) {
 }
 
 
-
-
-
+// game function
 function game() {
   let playerScore = 0;
   let computerScore = 0;
-  const buttons = document.querySelectorAll(".button");
+  // Add onclick function to each button
+  const buttons = document.querySelectorAll("button");
   buttons.forEach(button => {
     button.addEventListener("click", () => {
       let playerSelection = button.textContent;
       let computerSelection = getComputerChoice();
-      let result = (playRound(playerSelection, computerSelection));
-      let resultDiv = document.querySelector(".result");
-      resultDiv.textContent = result;
+      let result = playRound(playerSelection, computerSelection);
+      //Change choice text to result of round
+      const choice = document.querySelector(".choice");
+      choice.textContent = result;
+      // increment computer score if computer wins
+      if (result === "You Loose! Paper beats Rock" || result === "You Loose! Rock beats scissors" || result === "You Loose! Scissors beats paper") {
+        computerScore++;
+        const computer = document.querySelector(".computer-score");
+        computer.textContent = computerScore;
+      }
+      // increment player score if player wins
+      if (result === "You Win! Paper beats Rock" || result === "You Win! rock beats Scissors" || result === "You Win! Scissors beats paper") {
+        playerScore++;
+        const player = document.querySelector(".player-score");
+        player.textContent = playerScore;
+      }
     });
-  })
-  
-  // if (result === "You Win! Paper beats Rock" || result === "You Win! rock beats Scissors" || result === "You Win! Scissors beats paper")
-  //   playerScore++;
-  // if (result === "You Loose! Paper beats Rock" || result === "You Loose! Rock beats scissors" || result === "You Loose! Scissors beats paper")
-  //   computerScore++;
-
-
-  
-  // console.log("GAME OVER");
-  // console.log("------------------------------------------");
-  // if (playerScore > computerScore) return "Player Wins";
-  // else if (playerScore < computerScore) return "Computer Wins";
-  // return "That's a Tie";
+  });
 }
-game();
 
 
-
+game ();
 
 
